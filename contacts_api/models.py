8b4883e66913d24b2cc40ca9a57cbd713a6a0353
Joel Zisholtz <joelzisholtz@Joels-MBP.attlocal.net>
2021-02-15 23:54:50 -0600

    setup

--- contacts_api/models.py
+++ contacts_api/models.py
@@ -5,7 +5,12 @@ from django.db import models
 ## Contacts Model 
 class Contact(models.Model):
     name = models.CharField(max_length=100)
-    age = models.IntegerField()
+    email = models.CharField(max_length=100)
+    password = models.IntegerField()
+    phone_number = models.IntegerField()
+    postal_code= models.IntegerField()
 
     def __str__(self):
         return self.name
+        return self.email
+        return self.postal_code
